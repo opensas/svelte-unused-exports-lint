7e6c21e42aa03748460c0b00ac67581675e09b47
opensas <opensas@gmail.com>
2022-01-20 15:42:28 +0000

    comment call to capitalize

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { capitalize } from '$lib/utils';
+	//import { capitalize } from '$lib/utils';
 
 	const message = 'world';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<h2>Hello {capitalize(message)}</h2>
+<h2>Hello {message}</h2>
+<!-- <h2>Hello {capitalize(message)}</h2> -->
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
