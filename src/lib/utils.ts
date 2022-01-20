5749732ae7423df8588079c0dce2dc6d3145527e
opensas <opensas@gmail.com>
2022-01-20 12:36:10 -0300

    first commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,2 @@
+export const capitalize = (text: string): string =>
+	text.substring(0, 1).toUpperCase() + text.substring(1);
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { capitalize } from '$lib/utils';
+
+	const message = 'world';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<h2>Hello {capitalize(message)}</h2>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
